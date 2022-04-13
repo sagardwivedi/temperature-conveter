215a7cec484016dbe1bb3528e3decad07ffa3ca0
samy-2002 <sagar.dw20@gmail.com>
2022-04-13 19:29:34 +0530

    Added a header

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ function App() {
     };
 
     return (
-        <div className="flex h-screen w-full items-center justify-center bg-slate-400">
+        <div className="flex flex-col h-screen w-full items-center justify-center bg-slate-400">
+            <h1 className="text-4xl mb-20 font-bold">Converter</h1>
             <section className="w-[90%] max-w-lg rounded-2xl bg-white p-8  shadow-xl">
                 <form>
                     <div className="space-y-4">
@@ -51,15 +52,15 @@ function App() {
                         </div>
                         <div>
                             <button
-                                className="w-full rounded-lg border border-[#112691] bg-[#112691] px-4 py-2 text-xl text-white"
+                                className="w-full rounded-lg border border-[#112691]/60 bg-[#112691]/60 px-4 py-2 text-xl text-white hover:bg-[#112691]/70"
                                 onClick={convert}
                             >
                                 Convert
                             </button>
                         </div>
                     </div>
-                    <div className="flex flex-col space-y-2">
-                        <hr className="mt-4 h-0.5 bg-black" />
+                    <div className="flex flex-col space-y-1">
+                        <hr className="h-0.5 mt-2 bg-black" />
                         <span className="text-center text-xl font-semibold text-gray-900">
                             Result
                         </span>
