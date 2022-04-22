ffe2352e004fa974c78ff06b31264d96ae7e99c4
samy-2002 <sagar.dw20@gmail.com>
2022-04-23 00:07:09 +0530

    Made Visual Changes

--- src/App.js
+++ src/App.js
@@ -19,62 +19,70 @@ function App() {
     };
 
     return (
-        <div className="backg flex h-screen w-full flex-col items-center justify-center ">
-            <h1 className="mb-20 md:text-8xl text-4xl font-bold text-white">Converter</h1>
-            <section className="bg-transparent w-[90%] max-w-lg rounded-xl p-8 shadow">
-                <form>
-                    <div className="space-y-4">
-                        <div className="flex flex-col">
-                            <label
-                                htmlFor="temperature"
-                                className="text-xl font-semibold text-gray-900"
-                            >
-                                Temperature
-                            </label>
-                            <div className="flex items-center justify-between">
-                                <Input
-                                    temperature={temperature}
-                                    setTemperature={setTemperature}
-                                />
-                                <select className="mt-1 w-[12%] appearance-none rounded-lg border border-gray-400 p-2 text-center focus:outline-none">
-                                    <option value="Cel">°C</option>
-                                    <option value="Fah">F</option>
+        <>
+            <div className="backg flex h-screen w-full items-center justify-center">
+                <h1 className="animate-pulse text-8xl font-bold text-white">
+                    Converter
+                </h1>
+            </div>
+            <div className="h-screen">
+                <section className="w-[90%] max-w-lg rounded-xl bg-transparent p-8">
+                    <form>
+                        <div className="space-y-4">
+                            <div className="flex flex-col">
+                                <label
+                                    htmlFor="temperature"
+                                    className="text-xl font-semibold text-gray-900"
+                                >
+                                    Temperature
+                                </label>
+                                <div className="flex items-center justify-between">
+                                    <Input
+                                        temperature={temperature}
+                                        setTemperature={setTemperature}
+                                    />
+                                    <select className="mt-1 w-[10%] appearance-none rounded-lg border border-gray-400 p-2 text-center focus:outline-none">
+                                        <option value="Cel">°C</option>
+                                        <option value="Fah">F</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="flex flex-col">
+                                <label className="text-xl font-semibold text-gray-900">
+                                    Type
+                                </label>
+                                <select
+                                    className="mt-1 appearance-none rounded-lg border border-gray-400 p-2 focus:outline-none"
+                                    value={type}
+                                    onChange={(e) => settype(e.target.value)}
+                                >
+                                    <option value="">Select</option>
+                                    <option value="Fah">Fahrenheit</option>
+                                    <option value="Cel">Celsius</option>
                                 </select>
                             </div>
+                            <div>
+                                <button
+                                    className="w-full rounded-lg border border-[#112691]/60 bg-[#112691]/60 px-4 py-2 text-xl text-white hover:bg-[#112691]/70"
+                                    onClick={convert}
+                                >
+                                    Convert
+                                </button>
+                            </div>
                         </div>
-                        <div className="flex flex-col">
-                            <label className="text-xl font-semibold text-gray-900">
-                                Type
-                            </label>
-                            <select
-                                className="mt-1 appearance-none rounded-lg border border-gray-400 p-2 accent-purple-500 focus:outline-none"
-                                value={type}
-                                onChange={(e) => settype(e.target.value)}
-                            >
-                                <option value="">Select</option>
-                                <option value="Fah">Fahrenheit</option>
-                                <option value="Cel">Celsius</option>
-                            </select>
+                        <div className="flex flex-col space-y-1">
+                            <hr className="mt-2 h-0.5 bg-black" />
+                            <span className="text-center text-xl font-semibold text-gray-900">
+                                Result
+                            </span>
+                            <span className="text-center text-xl">
+                                {result}
+                            </span>
                         </div>
-                        <div>
-                            <button
-                                className="w-full rounded-lg border border-[#112691]/60 bg-[#112691]/60 px-4 py-2 text-xl text-white hover:bg-[#112691]/70"
-                                onClick={convert}
-                            >
-                                Convert
-                            </button>
-                        </div>
-                    </div>
-                    <div className="flex flex-col space-y-1">
-                        <hr className="mt-2 h-0.5 bg-black" />
-                        <span className="text-center text-xl font-semibold text-gray-900">
-                            Result
-                        </span>
-                        <span className="text-center text-xl">{result}</span>
-                    </div>
-                </form>
-            </section>
-        </div>
+                    </form>
+                </section>
+            </div>
+        </>
     );
 }
 
